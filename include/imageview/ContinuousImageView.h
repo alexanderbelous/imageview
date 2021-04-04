d29277ba4c59a65bd3842a741bb6ad449e486f15
Alexander Belous <alexander.belous@outlook.com>
2021-04-04 18:18:05 +0200

    Add iterators for ContinuousImageView.

--- include/imageview/ContinuousImageView.h
+++ include/imageview/ContinuousImageView.h
@@ -2,6 +2,7 @@
 
 #include <imageview/ImageRowView.h>
 #include <imageview/IsPixelFormat.h>
+#include <imageview/internal/ImageViewIterator.h>
 #include <imageview/internal/ImageViewStorage.h>
 #include <imageview/internal/PixelRef.h>
 
@@ -37,12 +38,31 @@ class ContinuousImageView {
 
   using byte_type = std::conditional_t<Mutable, std::byte, const std::byte>;
   using value_type = typename PixelFormat::color_type;
+  // If `Mutable == false`, then `reference` is an alias to `value_type`.
+  // Otherwise, it is a proxy class, which mimics `value_type&`:
+  // * is implicitly convertible to `value_type`.
+  // * you can assign a value_type to it, changing the color of the referenced pixel.
   using reference = std::conditional_t<Mutable, detail::PixelRef<PixelFormat>, value_type>;
+  // Constant LegacyInputIterator whose value_type is @value_type.
+  // It is not, however, a LegacyForwardIterator, because
+  //   std::iterator_traits<iterator>::reference
+  // is not const value_type&.
+  // Nevertheless, this iterator supports all arithmetic operations of LegacyRandomAccessIterator, e.g.,
+  //   `it += n`, ` --it`, `it[n]`.
+  using const_iterator = detail::ImageViewIterator<PixelFormat, false>;
+  // LegacyInputIterator whose value_type is @value_type. If Mutable == true, then it also
+  // models LegacyOutputIterator. It is not, however, a LegacyForwardIterator, because
+  //   std::iterator_traits<iterator>::reference
+  // is neither value_type& nor const value_type&.
+  // Nevertheless, this iterator supports all arithmetic operations of LegacyRandomAccessIterator, e.g.,
+  //   `it += n`, ` --it`, `it[n]`.
+  using iterator = detail::ImageViewIterator<PixelFormat, Mutable>;
 
   // Construct an empty view.
   // This constructor is only available if PixelFormat is default-constructible.
   template <class Enable = std::enable_if_t<std::is_default_constructible_v<PixelFormat>>>
   constexpr ContinuousImageView() noexcept(noexcept(std::is_nothrow_default_constructible_v<PixelFormat>)) {}
+
   // Construct a view into an image.
   // This constructor is only available if PixelFormat is default-constructible
   // \param height - height of the image.
@@ -55,6 +75,7 @@ class ContinuousImageView {
   template <class Enable = std::enable_if_t<std::is_default_constructible_v<PixelFormat>>>
   constexpr ContinuousImageView(unsigned int height, unsigned int width, gsl::span<byte_type> data) noexcept(
       std::is_nothrow_default_constructible_v<PixelFormat>);
+
   // Construct a view into an image.
   // \param height - height of the image.
   // \param width - width of the image.
@@ -66,6 +87,7 @@ class ContinuousImageView {
   // \param pixel_format - PixelFormat instance to use.
   constexpr ContinuousImageView(unsigned int height, unsigned int width, gsl::span<byte_type> data,
                                 const PixelFormat& pixel_format);
+
   // Construct a view into an image.
   // \param height - height of the image.
   // \param width - width of the image.
@@ -77,41 +99,53 @@ class ContinuousImageView {
   // \param pixel_format - PixelFormat instance to use.
   constexpr ContinuousImageView(unsigned int height, unsigned int width, gsl::span<byte_type> data,
                                 PixelFormat&& pixel_format);
+
   // Construct a read-only view from a mutable view.
   template <class Enable = std::enable_if_t<!Mutable>>
   constexpr ContinuousImageView(ContinuousImageView<PixelFormat, !Mutable> image);
+
   // Returns the height of the image.
   constexpr unsigned int height() const noexcept;
+
   // Returns the width of the image.
   constexpr unsigned int width() const noexcept;
+
   // Returns the total number of pixels.
   constexpr std::size_t area() const noexcept;
+
   // Returns true if the image has zero area, false otherwise.
   constexpr bool empty() const noexcept;
+
   // Returns the pixel format used by this image.
   constexpr const PixelFormat& pixelFormat() const noexcept;
+
   // Returns the pointer to the bitmap data.
   constexpr gsl::span<byte_type> data() const noexcept;
+
   // Returns the total size of the bitmap in bytes.
   constexpr std::size_t size_bytes() const noexcept;
+
+  // Returns an iterator to the first pixel.
+  constexpr iterator begin() const;
+
+  // Returns a const iterator to the first pixel.
+  constexpr const_iterator cbegin() const;
+
+  // Returns an iterator past the last pixel.
+  constexpr iterator end() const;
+
+  // Returns a const iterator past the last pixel.
+  constexpr const_iterator cend() const;
+
   // Access the specified pixel.
-  //
-  // If Mutable == true, then this function simply returns the color of the specified pixel.
-  // Otherwise, it returns a proxy object, which mimics value_type&:
-  // * is implicitly convertible to value_type, e.g.,
-  //
-  //   ContinuousImageView<PixelFormatRGB24, true> image(...);
-  //   const RGB24 color = image(1, 1);
-  //
-  // * you can assign a value_type to it, changing the color of the specified pixel:
-  //
-  //   ContinuousImageView<PixelFormatRGB24, true> image(...);
-  //   image(1, 1) = RGB24(255, 255, 0);
-  //
   // \param y - Y coordinate of the pixel. Should be within [0; height()).
   // \param x - X coordinate of the pixel. Should be within [0; width()).
+  // \return the color of the specified pixel.
   constexpr reference operator()(unsigned int y, unsigned int x) const;
+
   // Returns a non-owning view into the specified row of the image.
+  // \param y - 0-based index of the row. Should be within [0; height()).
+  // \return a non-owning view into the row @y.
   constexpr ImageRowView<PixelFormat, Mutable> row(unsigned int y) const;
 
  private:
@@ -195,6 +229,26 @@ constexpr auto ContinuousImageView<PixelFormat, Mutable>::data() const noexcept 
 template <class PixelFormat, bool Mutable>
 constexpr std::size_t ContinuousImageView<PixelFormat, Mutable>::size_bytes() const noexcept {
   return area() * PixelFormat::kBytesPerPixel;
+}
+
+template <class PixelFormat, bool Mutable>
+constexpr auto ContinuousImageView<PixelFormat, Mutable>::begin() const -> iterator {
+  return iterator(storage_.data(), pixelFormat());
+}
+
+template <class PixelFormat, bool Mutable>
+constexpr auto ContinuousImageView<PixelFormat, Mutable>::cbegin() const -> const_iterator {
+  return const_iterator(storage_.data(), pixelFormat());
+}
+
+template <class PixelFormat, bool Mutable>
+constexpr auto ContinuousImageView<PixelFormat, Mutable>::end() const -> iterator {
+  return iterator(storage_.data() + size_bytes(), pixelFormat());
+}
+
+template <class PixelFormat, bool Mutable>
+constexpr auto ContinuousImageView<PixelFormat, Mutable>::cend() const -> const_iterator {
+  return const_iterator(storage_.data() + size_bytes(), pixelFormat());
 }
 
 template <class PixelFormat, bool Mutable>
